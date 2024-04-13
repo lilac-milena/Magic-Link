1b8642b3db7aa41642027f6fe5c2fbb01bf6c42e
Lilac Milena <143427814+lilac-milena@users.noreply.github.com>
2024-04-13 04:58:55 +0000

    Add a request rate limit for the API (security) https://github.com/lilac-milena/Magic-Link/issues/9

--- api/main.js
+++ api/main.js
@@ -5,6 +5,7 @@
 
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const { rateLimit } = require('express-rate-limit')
 
 
 // 从环境变量中获取配置
@@ -240,6 +241,15 @@ app.all('*', function (req, res, next) {
     next();
 });
 
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 min
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 1 min).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+})
+
+// 将速率限制应用至所有 admin api 下
+app.use("/admin/api/*", limiter)
 app.get("/admin/api/*", async (req, res) => {
     let path = req.query.path
     let to = req.query.to
